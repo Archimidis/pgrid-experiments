fd326eaed988b7f582dc7e8e7f02b389a7ec0e6b
Vourlakis Nikolas <nvourlakis@gmail.com>
2012-01-24 23:26:02 +0200

    Project providing the code for the experiments.

---
+++ PGridController/src/grid/PGridController.java
@@ -0,0 +1,158 @@
+package grid;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.Key;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import pgrid.entity.EntityFactory;
+import pgrid.entity.EntityModule;
+import pgrid.entity.Host;
+import pgrid.entity.routingtable.RoutingTable;
+import pgrid.process.ControlProcess;
+import pgrid.process.ProcessModule;
+import pgrid.process.initialization.SystemInitializationProcess;
+import pgrid.service.LocalPeerContext;
+import pgrid.service.ServiceModule;
+import pgrid.service.ServiceRegistration;
+import pgrid.service.exchange.Exchange;
+import pgrid.service.repair.Repair;
+import pgrid.service.simulation.PersistencyException;
+import pgrid.service.simulation.Simulation;
+import pgrid.service.simulation.internal.XMLPersistencyService;
+import pgrid.service.simulation.spi.PersistencyDelegate;
+
+/**
+ *
+ * @author Vourlakis Nikolas
+ */
+public class PGridController {
+private static Map<String, Host> network_ = new HashMap<String, Host>();
+    private static final String PROMPT = "pgrid> ";
+
+    public static RoutingTable load(String file) throws PersistencyException, FileNotFoundException {
+        RoutingTable routingTable = new RoutingTable();
+        PersistencyDelegate bootstrapService = new XMLPersistencyService();
+        bootstrapService.load(file, routingTable);
+        return routingTable;
+    }
+
+    // arg[0] -> xml file where the controller's routing table will be loaded from.
+    // arg[1] -> network file with all the hosts in the network.
+    public static void main(String[] args) throws IOException {
+//        LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
+//        StatusPrinter.print(lc);
+        Logger logger_ = LoggerFactory.getLogger(PGridController.class);
+        logger_.info("Initializing Controller");
+//        args = new String[]{
+//                "experiments/Topology1/controller.xml",
+//                "experiments/Topology1/network"};
+        if (args.length != 2) {
+            logger_.error("No file given containing the hots of the network.");
+            System.exit(1);
+        }
+
+        RoutingTable rt = null;
+        try {
+            rt = load(args[0]);
+        } catch (PersistencyException ignored) {
+            System.exit(1);
+        }
+
+        final Injector injector = Guice.createInjector(
+                new EntityModule(),
+                new ServiceModule(rt.getLocalhost().getAddress().getHostName(), rt.getLocalhost().getPort(), 10),
+                new ProcessModule());
+
+        LocalPeerContext context = injector.getInstance(LocalPeerContext.class);
+        context.setRoutingTable(rt);
+
+        SystemInitializationProcess initProcess =
+                injector.getInstance(SystemInitializationProcess.class);
+        try {
+            //initProcess.load(args[0]);
+            ServiceRegistration[] registrations = {
+                    injector.getInstance(Key.get(ServiceRegistration.class, Exchange.class)),
+                    injector.getInstance(Key.get(ServiceRegistration.class, Repair.class)),
+                    injector.getInstance(Key.get(ServiceRegistration.class, Simulation.class))};
+            initProcess.serviceRegistration(registrations);
+        } catch (Exception e) {
+            System.out.println("Error during service registration. " + e.getMessage());
+            System.exit(2);
+        }
+        initProcess.startServer();
+
+
+
+//        try {
+//            Thread.sleep(2000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+
+        ControlProcess control = injector.getInstance(ControlProcess.class);
+        control.loadNetworkFile(args[1], injector.getInstance(EntityFactory.class));
+//
+        System.out.println();
+        System.out.println("PGrid  Copyright (C) 2012  Vourlakis Nikolas\n" +
+                "This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.\n" +
+                "This is free software, and you are welcome to redistribute it\n" +
+                "under certain conditions; type `show c' for details.");
+        System.out.println();
+
+        System.out.println("Controller runs on " +
+                context.getLocalRT().getLocalhost() + ":" +
+                context.getLocalRT().getLocalhost().getPort());
+
+        BufferedReader x = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+
+        System.out.print(PROMPT);
+        System.out.flush();
+        while ((line = x.readLine()) != null) {
+            String[] lineSplit = line.split(" ");
+            String commandName = lineSplit[0];
+            for (int i = 0; i < lineSplit.length; i++) {
+                lineSplit[i] = lineSplit[i].trim();
+            }
+
+            if (commandName.equals("exit")) {
+                control.exit();
+                System.out.flush();
+                break;
+            } else if (commandName.equals("info")) {
+                control.info();
+            } else if (commandName.equals("killhost")) {
+                if (lineSplit.length == 2) {
+                    String hostname = lineSplit[1];
+                    control.killHost(hostname);
+                } else {
+                    System.out.println("A host name must follow the 'killhost' command.");
+                }
+            } else if (commandName.equals("repair")) {
+                if (lineSplit.length == 5 && lineSplit[1].equals("-i") && lineSplit[3].equals("-f")) {
+                    // repair -i <hostname> -f <hostname>
+                    control.forceRepairSingleHost(lineSplit[2], lineSplit[4]);
+                } else if (lineSplit.length == 5 && lineSplit[1].equals("-i") && lineSplit[3].equals("-p")) {
+                    // repair  -i <hostname> -p <subtree path>
+                    control.forceRepairSubtree(lineSplit[2], lineSplit[4]);
+                } else {
+                    System.out.println(commandName + ": malformed command");
+                    System.out.println("'repair -i <hostname> -f <hostname>'");
+                    System.out.println("repair -i <hostname> -p <subtree path>");
+                    System.out.println("Where -i: initiator name");
+                    System.out.println("\t -f: the failed host name");
+                    System.out.println("\t -p: the path of the failed subtree");
+                }
+            }
+            System.out.print(PROMPT);
+            System.out.flush();
+        }
+    }
+}
---
+++ PGridPeer/src/grid/GridPeer.java
@@ -0,0 +1,119 @@
+/*
+ * This file is part of the pgrid project.
+ *
+ * Copyright (c) 2012. Vourlakis Nikolas. All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package grid;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.Key;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import pgrid.entity.EntityModule;
+import pgrid.entity.Host;
+import pgrid.entity.routingtable.RoutingTable;
+import pgrid.process.ProcessModule;
+import pgrid.process.initialization.SystemInitializationProcess;
+import pgrid.service.LocalPeerContext;
+import pgrid.service.ServiceModule;
+import pgrid.service.ServiceRegistration;
+import pgrid.service.exchange.Exchange;
+import pgrid.service.repair.Repair;
+import pgrid.service.simulation.PersistencyException;
+import pgrid.service.simulation.Simulation;
+import pgrid.service.simulation.internal.XMLPersistencyService;
+import pgrid.service.simulation.spi.PersistencyDelegate;
+
+import java.io.FileNotFoundException;
+import java.net.UnknownHostException;
+
+/**
+ * A peer that initializes the services needed for simulation and then it waits
+ * patiently for orders by the controller of the experiments.
+ * This code will be run on each node of the grid system of TUC.
+ * The file where the routing table is stored for this peer must be given in
+ * order to initialize properly and take a place to the pgrid topology.
+ *
+ * @author Vourlakis Nikolas <nvourlakis@gmail.com>
+ */
+public class GridPeer {
+
+    private static final Logger logger_ = LoggerFactory.getLogger(GridPeer.class);
+
+    public static RoutingTable load(String file) throws PersistencyException, FileNotFoundException {
+        RoutingTable routingTable = new RoutingTable();
+        PersistencyDelegate bootstrapService = new XMLPersistencyService();
+        bootstrapService.load(file, routingTable);
+        return routingTable;
+    }
+
+    // arg[0] -> xml file where the peer's routing table will be loaded from.
+    public static void main(String[] args) throws UnknownHostException {
+        if (args.length != 1) {
+            logger_.error("No file given to load and store routing table.");
+            System.exit(1);
+        }
+
+        RoutingTable rt = null;
+        try {
+            rt = load(args[0]);
+        } catch (PersistencyException e) {
+            logger_.error("{}", e);
+            System.exit(1);
+        } catch (FileNotFoundException e) {
+            logger_.error("{}", e);
+            System.exit(1);
+        }
+
+        Injector injector = Guice.createInjector(
+                new EntityModule(),
+                new ServiceModule(rt.getLocalhost().getAddress().getHostName(), rt.getLocalhost().getPort(), 10),
+                new ProcessModule());
+
+        LocalPeerContext context = injector.getInstance(LocalPeerContext.class);
+        context.setRoutingTable(rt);
+
+        SystemInitializationProcess initProcess =
+                injector.getInstance(SystemInitializationProcess.class);
+
+        try {
+            initProcess.load(args[0]);
+            ServiceRegistration[] registrations = {
+                    injector.getInstance(Key.get(ServiceRegistration.class, Exchange.class)),
+                    injector.getInstance(Key.get(ServiceRegistration.class, Repair.class)),
+                    injector.getInstance(Key.get(ServiceRegistration.class, Simulation.class))
+            };
+            initProcess.serviceRegistration(registrations);
+        } catch (Exception e) {
+            logger_.error("Error during service registration. {}", e);
+            System.exit(2);
+        }
+
+        Host localhost = context.getLocalRT().getLocalhost();
+
+        logger_.debug("Localhost");
+        logger_.debug("[{}] {}:{} - '{}'", new Object[]{localhost.getHostPath(), localhost, localhost.getPort(), localhost.getUUID()});
+        logger_.debug("Routing Table");
+        for (Host host : context.getLocalRT().getAllHosts()) {
+            logger_.debug("[{}] {}:{} - '{}'", new Object[]{host.getHostPath(), host, host.getPort(), host.getUUID()});
+        }
+
+        initProcess.startServer();
+        // Waiting orders from the controller!
+    }
+}
